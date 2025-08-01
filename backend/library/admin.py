4220d33d4cf751e22fca4d1c97c22628bb9b7bee
hijbullahx <hijbullah119445@gmail.com>
2025-08-01 08:11:47 +0600

    Initial project structure with Django

--- backend/library/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Student, LibraryEntry, ELibraryPC, ELibraryUsage
-
-admin.site.register(Student)
-admin.site.register(LibraryEntry)
-admin.site.register(ELibraryPC)
-admin.site.register(ELibraryUsage)
