35b6149f03a6fe8dc778ba8fee89e0918bf98167
hijbullahx <hijbullah119445@gmail.com>
2025-08-06 07:36:14 +0600

    vercel settings

--- library_automation/settings.py
+++ library_automation/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+import os
+import dj_database_url
 
 
 # Quick-start development settings - unsuitable for production
@@ -75,10 +77,9 @@ WSGI_APPLICATION = "library_automation.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
