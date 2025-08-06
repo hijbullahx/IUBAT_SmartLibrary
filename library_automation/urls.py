0e2ea1cdcc35c4ffc8c7605802607ee61de90aca
MD TAHER BIN OMAR HIJBULLAH <hijbullah119445@gmail.com>
2025-08-06 10:20:05 +0600

    Update urls.py

--- library_automation/urls.py
+++ library_automation/urls.py
@@ -1,8 +1,13 @@
 # library_automation/urls.py
 from django.contrib import admin
-from django.urls import path, include # <-- Add 'include' here
+from django.urls import path, include 
+from django.urls import re_path
+from django.views.generic import TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('library.urls')), # <-- Add this line
-]
+    path('api/', include('library.urls')),
+    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
