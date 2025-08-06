9608cf96ba00a27d7ef0b4acd02b61cbee62048c
hijbullahx <hijbullah119445@gmail.com>
2025-08-06 18:25:37 +0600

    Fix database configuration for Vercel deployment

--- library_automation/settings.py
+++ library_automation/settings.py
@@ -81,11 +81,19 @@ WSGI_APPLICATION = "library_automation.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'))
-    )
-}
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(default=DATABASE_URL)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
